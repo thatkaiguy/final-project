4d2cef1cbba92cc8958867c4c40493ec53a140c4
thatkaiguy <kai438@gmail.com>
2015-08-17 22:36:49 -0700

    search params

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -1,11 +1,19 @@
 class Api::SearchesController < ApplicationController
   def index
-    
-    @activities = Activity.joins(:address)
-      .where('addresses.city ILIKE ?', "%#{params[:city]}%")
+    if params[:city] && params[:category]
+      @activities = Activity.joins(:address, :categories)
+        .where('addresses.city ILIKE ?', "%#{params[:city]}%")
+        .where('categories.label ILIKE ?', "%#{params[:category]}%")
+    elsif params[:city]
+      @activities = Activity.joins(:address)
+        .where('addresses.city ILIKE ?', "%#{params[:city]}%")
+    elsif params[:category]
+      @activities = Activity.joins(:categories)
+        .where('categories.label ILIKE ?', "%#{params[:category]}%")
+    end
 
     if @activities.nil?
-      @activities = Activity.all
+      @activities = [Activity.new]
     end
 
     render :index
