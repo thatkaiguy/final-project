edf63bc9fa09bda475b61c1effc7c385a23189fa
thatkaiguy <kai438@gmail.com>
2015-08-22 12:08:30 -0700

    search index featured header

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  before_action :check_login
+  # before_action :check_login
 
   def check_login
     redirect_to new_session_url unless logged_in?
